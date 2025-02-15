6772bcd58573ec041996ccd166e5cc5da20804b9
Rita <ritaafanaseva@icloud.com>
2025-02-15 18:07:30 +0300

    add analyzer

--- src/app/providers/ErrorBoundary/ui/ThrowErrorButton.tsx
+++ src/app/providers/ErrorBoundary/ui/ThrowErrorButton.tsx
@@ -2,6 +2,7 @@ import {CustomButton} from "shared/ui/CustomButton/CustomButton";
 import {useTranslation} from "react-i18next";
 import {useEffect, useState} from "react";
 
+
 export const ThrowErrorButton = () => {
     const [error, setError] = useState(false);
 
