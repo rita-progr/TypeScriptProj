b8d1c9322e1e451ceddde0a68a576940a62f3f6c
Rita <ritaafanaseva@icloud.com>
2025-02-23 10:32:27 +0300

    change loaders

--- config/build/BuildLoaders.tsx
+++ config/build/BuildLoaders.tsx
@@ -1,7 +1,6 @@
 import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
-import buildCssLoader from "./loaders/buildCssLoader";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function BuildLoaders(options:BuildOptions):webpack.RuleSetRule[] {
 
@@ -31,7 +30,22 @@ export function BuildLoaders(options:BuildOptions):webpack.RuleSetRule[] {
         }
     }
 
-    const typesStyles = buildCssLoader(options.isDev)
+    const typesStyles = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath:string)=>Boolean(resPath.includes(".module.")),
+                        localIdentName: options.isDev ? "[path][name]__[local]--[hash:base64:5]" : "hash:base64:8",
+                    },
+                }
+            },
+            "sass-loader",
+        ],
+    }
 
     const typescriptLoader = {
             test: /\.tsx?$/,
