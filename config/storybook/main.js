bcd7baa8ab5e5ad627b298192cbb55c9b69e9930
Rita <ritaafanaseva@icloud.com>
2025-02-23 11:26:47 +0300

    fixed storybook config

---
+++ config/storybook/main.js
@@ -0,0 +1,16 @@
+const config = {
+  stories: [ "../../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-webpack5-compiler-babel"
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  }
+};
+export default config;
