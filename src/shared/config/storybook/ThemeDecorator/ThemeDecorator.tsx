5bcd23a8f63a413e57d05f8df5298a99f15c813f
Rita <ritaafanaseva@icloud.com>
2025-02-23 10:51:42 +0300

    fixed story decorators ThemeDecorator

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,7 +1,13 @@
-import {ThemeType} from "app/providers/ThemeProvider";
+import React from 'react';
+import { ThemeType } from 'app/providers/ThemeProvider';
 
-export const ThemeDecorator = (theme: ThemeType)=> (Story:  React.ComponentType) => (
-    <div className={`app ${theme}`}>
-        <Story />
-    </div>
-);
+export function ThemeDecorator(theme: ThemeType) {
+    return function Decorator(Story: React.ComponentType) {
+        return (
+            <div className={`app ${theme}`}>
+                <Story />
+            </div>
+        );
+    };
+}
+
