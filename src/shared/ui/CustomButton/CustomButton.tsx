819d859c4d238fa67cc8184f670372002c37c451
Rita <ritaafanaseva@icloud.com>
2025-02-25 07:55:49 +0300

    upgrade sidebar, buttons and more

--- src/shared/ui/CustomButton/CustomButton.tsx
+++ src/shared/ui/CustomButton/CustomButton.tsx
@@ -4,12 +4,21 @@ import {ButtonHTMLAttributes, FC} from "react";
 
 export enum ThemeButton{
     CLEAR='clear',
-    OUTLINE = 'outline'
+    OUTLINE = 'outline',
+    BACKGROUND = 'background_theme',
+    BACKGROUND_INVERTED = 'backgroundInverted_theme',
+}
+
+export enum ButtonSize{
+    LARGE= 'l',
+    MEDIUM='m',
+    SMALL='s'
 }
 
 interface CustomButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
     theme?:ThemeButton
+    size?:ButtonSize
 }
 
 export const CustomButton:FC<CustomButtonProps> = (props) => {
@@ -17,11 +26,17 @@ export const CustomButton:FC<CustomButtonProps> = (props) => {
     const {
         className,
         children,
+        size,
         theme = ThemeButton.CLEAR,
         ...otherProps
     } = props;
+
+    const mods: Record<string,boolean> = {
+
+    }
+
     return (
-        <button className={classNames(cls.CustomButton, {},[className, cls[theme] ])}
+        <button className={classNames(cls.CustomButton, mods,[className, cls[size], cls[theme]  ])}
                 {...otherProps}>
             {children}
         </button>
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,5 @@
 import cls from './Navbar.module.scss';
 import {classNames} from "shared/lib/classNames/classNames";
-import {CustomLink} from "shared/ui/CustomLink/CustomLink";
 
 interface NavbarProps{
     className?: string;
@@ -9,12 +8,7 @@ interface NavbarProps{
 export const Navbar = ({className}:NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {},[className])}>
-            <CustomLink to={'/'}>
-                Главная
-            </CustomLink>
-            <CustomLink to={'/about'}>
-                О нас
-            </CustomLink>
+            /
         </div>
     )
 }
