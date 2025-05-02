3ebb1e7887224bcd87e73537a151a0a086ee80a1
Rita <ritaafanaseva@icloud.com>
2025-05-02 11:30:33 +0300

    fixed bugs

--- src/app/providers/route/ui/RequireAuth.tsx
+++ src/app/providers/route/ui/RequireAuth.tsx
@@ -13,17 +13,19 @@ export function RequireAuth({children, roles}:RequireAuthProps) {
     const auth = useSelector(getUserAuthData);
     const location = useLocation();
     const userRoles = useSelector(userRolesSelector);
-console.log(roles,userRoles )
+
     const hasRequiredRoles= useMemo(()=>{
-        if(!userRoles) return true;
+        if(!roles) return true;
         return roles?.some(requiredRole => {
-            console.log(userRolesSelector)
-            console.log(userRoles?.includes(requiredRole))
             return userRoles?.includes(requiredRole);
         })
     },[roles, userRoles])
 
-    if(!auth || !hasRequiredRoles){
+    if(!hasRequiredRoles) {
+        return <Navigate to={RoutePath.main} state = {{from:location}} replace/>
+    }
+
+    if(!auth){
         return <Navigate to={RoutePath.main} state = {{from:location}} replace/>
     }
 
